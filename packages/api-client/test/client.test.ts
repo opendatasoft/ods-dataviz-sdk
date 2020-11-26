c1c835beba74847c8cd545ece04b3252c1cd5ec8
rkouye <romaric.kouye@opendatasoft.com>
2020-11-26 18:36:48 +0100

    Basic testing

--- packages/api-client/test/client.test.ts
+++ packages/api-client/test/client.test.ts
@@ -1,8 +1,31 @@
+import { enableFetchMocks } from 'jest-fetch-mock';
 import { ApiClient } from '../src';
+import { expect, it, beforeEach } from '@jest/globals';
+enableFetchMocks();
 
-test('should create a client without error', () => {
-  new ApiClient();
-  new ApiClient({
-    domain: 'public',
+beforeEach(() => {
+  fetchMock.resetMocks();
+});
+
+it('should create a client without error', () => {
+  const client = new ApiClient();
+  expect(client.defaultConfig.baseUrl).toEqual('/api/v2/');
+});
+
+it('should use the global fetch', () => {
+  const client = new ApiClient();
+  const globalFetch = global.fetch;
+  expect(client.defaultConfig.fetch).toEqual(globalFetch);
+});
+
+it('should call fetch with the right url', async () => {
+  const apiResponse = { any: 'any' };
+  fetchMock.once(async req => {
+    expect(req.url).toEqual('https://public.opendatasoft.com/api/v2/catalog/');
+    return JSON.stringify(apiResponse);
   });
+  const client = new ApiClient({ domain: 'public' });
+  const response = await client.get('catalog/');
+  expect(response).toEqual(apiResponse);
+  expect(fetch).toHaveBeenCalledTimes(1);
 });
