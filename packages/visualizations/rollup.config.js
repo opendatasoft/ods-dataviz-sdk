b038b5db73e68630163c8ebf0c022331cb8b3df8
rkouye <romaric.kouye@opendatasoft.com>
2021-01-11 13:13:43 +0100

    Enable run-time check for svelte development only

--- packages/visualizations/rollup.config.js
+++ packages/visualizations/rollup.config.js
@@ -5,6 +5,8 @@ import autoprefixer from 'autoprefixer';
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
+const production = !process.env.ROLLUP_WATCH;
+
 const preprocess = autoPreprocess({
     scss: {
         includePaths: ['src'],
@@ -23,6 +25,8 @@ export default {
     },
     plugins: [
         svelte({
+            // enable run-time checks when not in production
+			dev: !production,
             include: 'src/**/*.svelte',
             preprocess,
         }),
