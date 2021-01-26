2e953eb4b36d72c7c0f580d6c0de2bac8fed2fdf
William Mai <contact@williammai.fr>
2021-01-26 10:27:58 +0100

    fix: put the UMD bundle into the `dist/umd/` folder

--- packages/api-client/tsdx.config.js
+++ packages/api-client/tsdx.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-    rollup(config, options) {
+    rollup(config) {
         // This configuration allows 'immutability-helper' to be included in the UMD bundle.
         // Related issue: https://github.com/formium/tsdx/issues/179
         if (config.output.format === 'umd') {
-          delete config.external;
+            delete config.external;
+            config.output = {
+                    name: 'opendatasoft.apiClient',
+                    file: __dirname + '/dist/umd/opendatasoft.apiclient.umd.js',
+                    name: 'opendatasoft.apiClient',
+                    format: 'umd'
+            }
         }
         return config;
     }
