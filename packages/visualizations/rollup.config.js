3f77a72de0869a74f612f0c73cf232ad9c1c7ef5
Benoit Richter <benoit.richter@opendatasoft.com>
2021-03-09 13:45:48 +0100

    visualizations: Transpile output to ES5

--- packages/visualizations/rollup.config.js
+++ packages/visualizations/rollup.config.js
@@ -4,6 +4,7 @@ import autoprefixer from 'autoprefixer';
 
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { babel } from '@rollup/plugin-babel';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -38,5 +39,14 @@ export default {
             rootDir: 'src',
         }),
         nodeResolve(),
+        babel({
+            babelHelpers: 'bundled',
+            extensions: ['.ts', '.mjs', '.js', '.svelte'],
+            include: [
+                'src/**',
+                'node_modules/**',
+            ],
+            presets: ['@babel/preset-env'],
+        }),
     ],
 };
