a37421125b5c6620d4e0dca6f3c2da234d2a86b8
Benoit Richter <benoit.richter@opendatasoft.com>
2021-05-03 09:58:01 +0200

    Update packages/visualizations/src/components/MarkdownText/index.ts
    Co-authored-by: Romaric <20905904+rkouye@users.noreply.github.com>

--- packages/visualizations/src/components/MarkdownText/index.ts
+++ packages/visualizations/src/components/MarkdownText/index.ts
@@ -2,7 +2,7 @@ import type { MarkdownTextOptions } from '../types';
 import MarkdownTextImpl from './MarkdownText.svelte';
 import { SvelteImpl } from '../SvelteImpl';
 
-export default class Chart extends SvelteImpl<string, MarkdownTextOptions> {
+export default class MarkdownText extends SvelteImpl<string, MarkdownTextOptions> {
     protected getSvelteComponentClass(): typeof MarkdownTextImpl {
         return MarkdownTextImpl;
     }
