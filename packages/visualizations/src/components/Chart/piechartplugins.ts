de7bea76e360abf871f2b68b89f018647c79b108
William Mai <contact@williammai.fr>
2021-09-20 16:39:34 +0200

    fix: ChartJS plugins must have an ID
    Documentation: https://www.chartjs.org/docs/latest/developers/plugins.html#plugin-id

--- packages/visualizations/src/components/Chart/piechartplugins.ts
+++ packages/visualizations/src/components/Chart/piechartplugins.ts
@@ -8,61 +8,61 @@ interface ChartPluginDataLabelArc extends ArcElement {
     outerRadius: number; // FIXME: This properties should be part of `ArcProps`, I don't understand why I had to add it here
 }
 
-export const pieDataLabelsPlugin =
-{
-        afterDraw: (chart: Chart<'pie'>) => {
-            const ctx = chart.ctx;
-            ctx.save();
-            const chartCenterPoint = {
-                x:
-                    (chart.chartArea.right - chart.chartArea.left) / 2 +
-                    chart.chartArea.left,
-                y: (chart.chartArea.bottom - chart.chartArea.top) / 2 + chart.chartArea.top,
-            };
+export const pieDataLabelsPlugin = {
+    id: 'chartjs-plugin-datalabels',
+    afterDraw: (chart: Chart<'pie'>) => {
+        const ctx = chart.ctx;
+        ctx.save();
+        const chartCenterPoint = {
+            x:
+                (chart.chartArea.right - chart.chartArea.left) / 2 +
+                chart.chartArea.left,
+            y: (chart.chartArea.bottom - chart.chartArea.top) / 2 + chart.chartArea.top,
+        };
 
-            const { labels } = chart.config.data as DataLabelsOptions;
-            const backgroundColor = chart.config.data.datasets[0].backgroundColor as string[];
+        const { labels } = chart.config.data as DataLabelsOptions;
+        const backgroundColor = chart.config.data.datasets[0].backgroundColor as string[];
 
-            if (labels && labels.length ) {
-                for (let i = 0; i < labels.length; i++) {
-                    // FIXME: Why can't I simply cast using `as ArcElement` here?
-                    const arc = chart.getDatasetMeta(0).data[i] as unknown as ChartPluginDataLabelArc;
+        if (labels && labels.length ) {
+            for (let i = 0; i < labels.length; i++) {
+                // FIXME: Why can't I simply cast using `as ArcElement` here?
+                const arc = chart.getDatasetMeta(0).data[i] as unknown as ChartPluginDataLabelArc;
 
-                    const centerPoint = arc.getCenterPoint();
+                const centerPoint = arc.getCenterPoint();
 
-                    // Get the center of the chart and the center point of the slice to determine the angle
+                // Get the center of the chart and the center point of the slice to determine the angle
 
-                    const angle = Math.atan2(
-                        centerPoint.y - chartCenterPoint.y,
-                        centerPoint.x - chartCenterPoint.x
-                    );
+                const angle = Math.atan2(
+                    centerPoint.y - chartCenterPoint.y,
+                    centerPoint.x - chartCenterPoint.x
+                );
 
-                    //Check for offset on datalabels
-                    let datalabelsOffset = 0;
-                    if (arc.$datalabels[0]._model?.offset) {
-                        datalabelsOffset = arc.$datalabels[0]._model.offset
-                    };
+                //Check for offset on datalabels
+                let datalabelsOffset = 0;
+                if (arc.$datalabels[0]._model?.offset) {
+                    datalabelsOffset = arc.$datalabels[0]._model.offset
+                };
 
-                    // Get a first drawing point
-                    const point1X = chartCenterPoint.x + Math.cos(angle) * (arc.outerRadius - 8);
-                    let point1Y = chartCenterPoint.y + Math.sin(angle) * (arc.outerRadius - 8);
-                    // Get the second point depending of its position
-                    const point2X =
-                        chartCenterPoint.x + Math.cos(angle) * (arc.outerRadius + 4 + datalabelsOffset);
-                    let point2Y = chartCenterPoint.y + Math.sin(angle) * (arc.outerRadius + 4 + datalabelsOffset);
+                // Get a first drawing point
+                const point1X = chartCenterPoint.x + Math.cos(angle) * (arc.outerRadius - 8);
+                let point1Y = chartCenterPoint.y + Math.sin(angle) * (arc.outerRadius - 8);
+                // Get the second point depending of its position
+                const point2X =
+                    chartCenterPoint.x + Math.cos(angle) * (arc.outerRadius + 4 + datalabelsOffset);
+                let point2Y = chartCenterPoint.y + Math.sin(angle) * (arc.outerRadius + 4 + datalabelsOffset);
 
-                    // Draw line between center of the slice and label
-                    if (backgroundColor && backgroundColor.length) {
-                        ctx.strokeStyle = backgroundColor[i];
-                    }
-                    ctx.beginPath();
-                    ctx.lineWidth = 2;
-                    ctx.moveTo(point1X, point1Y);
-                    ctx.lineTo(point2X, point2Y);
-                    ctx.stroke();
+                // Draw line between center of the slice and label
+                if (backgroundColor && backgroundColor.length) {
+                    ctx.strokeStyle = backgroundColor[i];
                 }
+                ctx.beginPath();
+                ctx.lineWidth = 2;
+                ctx.moveTo(point1X, point1Y);
+                ctx.lineTo(point2X, point2Y);
+                ctx.stroke();
             }
+        }
 
-            ctx.restore();
-        },
-    };
+        ctx.restore();
+    },
+};
