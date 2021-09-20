1ebd9ffc77da9b9e07921406a6163e7b4d477be9
rkouye <romaric.kouye@opendatasoft.com>
2021-09-20 16:34:37 +0200

    Add linting with eslint, prettier and svelte check

---
+++ packages/visualizations/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    plugins: ['svelte3', '@typescript-eslint'],
+    overrides: [
+        {
+            files: ['*.svelte'],
+            processor: 'svelte3/svelte3',
+        },
+    ],
+    settings: {
+        'svelte3/typescript': true,
+    },
+    rules: {},
+};
