322685c52ba84b64713e05fa7d619eacb71f0023
William Mai <contact@williammai.fr>
2021-09-22 09:56:57 +0200

    style: Remove unnecessary `else`

--- packages/visualizations/src/utils.ts
+++ packages/visualizations/src/utils.ts
@@ -3,10 +3,10 @@ const formatter = new Intl.NumberFormat(undefined, {
     notation: 'compact',
 } as any);
 
+// eslint-disable-next-line import/prefer-default-export
 export function compactStringOrNumber(value: string | number) {
     if (typeof value === 'string') {
         return value.length > 10 ? `${value.slice(0, 10)}...` : value
-    } else {
-        return formatter.format(value).toString();
     }
+    return formatter.format(value).toString();
 }
