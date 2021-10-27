677a49d52a38058682ea4915fd19173202c5be24
Etienne Burdet <etienne.burdet@gmail.com>
2021-10-27 15:06:53 +0200

    Fix: remove disabled option for chart (studio only)

--- packages/visualizations/src/components/SourceLink/SourceLink.svelte
+++ packages/visualizations/src/components/SourceLink/SourceLink.svelte
@@ -2,19 +2,15 @@
     import type { Source } from '../types';
 
     export let source: Source;
-    const { href, label = 'View source', disabled = false } = source;
+    const { href, label = 'View source' } = source;
 </script>
 
-<a {href} target="_blank" rel="noopener norefer" role="link" class:disabled>
+<a {href} target="_blank" rel="noopener norefer" role="link">
     {label}
 </a>
 
 <style>
     a {
         font-size: var(--chart-source-font-size, 12px);
-    }
-
-    a.disabled {
-        pointer-events: none;
     }
 </style>
