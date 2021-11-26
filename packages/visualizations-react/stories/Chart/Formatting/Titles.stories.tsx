cb18e5f378ac36f728141b3d5f40f66ee15df300
rkouye <romaric.kouye@opendatasoft.com>
2021-11-26 10:59:34 +0100

    Add stories on text formatting

---
+++ packages/visualizations-react/stories/Chart/Formatting/Titles.stories.tsx
@@ -0,0 +1,112 @@
+import { ChartOptions, DataFrame, compactStringOrNumber } from '@opendatasoft/visualizations';
+import { Meta } from '@storybook/react';
+import { Props } from '../../../src';
+import { defaultSource } from '../../utils';
+import { Sample } from '../Chart.stories';
+import { storyWithArgs } from '../../utils';
+
+const meta: Meta = {
+    title: 'Chart/Formatting/Titles',
+};
+
+export default meta;
+
+export const LineLongTitle = storyWithArgs<Props<DataFrame, ChartOptions>>(Sample, {
+    data: {
+        loading: false,
+        value: [
+            { x: 0, y: 240000000, z: 1 },
+            { x: 1, y: -14000000, z: 2 },
+            { x: 2, y: 200000000, z: 3 },
+            { x: 3, y: 300000, z: 1 },
+            { x: 4, y: 1800000.47, z: 1 },
+            { x: 5, y: 778000, z: 1 },
+        ],
+    },
+    options: {
+        labelColumn: 'x',
+        source: defaultSource,
+        ariaLabel: 'Line chart with long title',
+        series: [
+            {
+                type: 'line',
+                valueColumn: 'y',
+                tension: 0,
+                borderColor: 'rgb(22, 161, 145)',
+                pointRadius: 0,
+                dataLabels: {
+                    display: true,
+                    align: function (index, { dataFrame }) {
+                        if (dataFrame[index].y >= 0) {
+                            return 'end';
+                        } else {
+                            return 'start';
+                        }
+                    },
+                    anchor: function (index, { dataFrame }) {
+                        if (dataFrame[index].y >= 0) {
+                            return 'end';
+                        } else {
+                            return 'start';
+                        }
+                    },
+                    color: 'rgb(22, 161, 145)',
+                },
+            },
+        ],
+        title: {
+            text:
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla optio unde porro incidunt provident quis amet saepe voluptas explicabo maiores. Voluptatum itaque dolores consectetur assumenda repellendus accusamus laudantium vero deleniti.',
+        },
+        subtitle: {
+            text:
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla optio unde porro incidunt provident quis amet saepe voluptas explicabo maiores. Voluptatum itaque dolores consectetur assumenda repellendus accusamus laudantium vero deleniti.',
+        },
+    },
+});
+
+export const LineNoTitle = storyWithArgs<Props<DataFrame, ChartOptions>>(Sample, {
+    data: {
+        loading: false,
+        value: [
+            { x: 0, y: 240000000, z: 1 },
+            { x: 1, y: -14000000, z: 2 },
+            { x: 2, y: 200000000, z: 3 },
+            { x: 3, y: 300000, z: 1 },
+            { x: 4, y: 1800000.47, z: 1 },
+            { x: 5, y: 778000, z: 1 },
+        ],
+    },
+    options: {
+        labelColumn: 'x',
+        source: defaultSource,
+        ariaLabel: 'Line chart without title',
+        series: [
+            {
+                type: 'line',
+                valueColumn: 'y',
+                tension: 0,
+                borderColor: 'rgb(22, 161, 145)',
+                pointRadius: 0,
+                dataLabels: {
+                    display: true,
+                    align: function (index, { dataFrame }) {
+                        if (dataFrame[index].y >= 0) {
+                            return 'end';
+                        } else {
+                            return 'start';
+                        }
+                    },
+                    anchor: function (index, { dataFrame }) {
+                        if (dataFrame[index].y >= 0) {
+                            return 'end';
+                        } else {
+                            return 'start';
+                        }
+                    },
+                    color: 'rgb(22, 161, 145)',
+                },
+            },
+        ],
+    },
+});
