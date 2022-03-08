c46a9458c649731f23c1a641db46d3c38d8a148b
JulietteRevOpendatasoft <98836637+JulietteRevOpendatasoft@users.noreply.github.com>
2022-03-08 09:33:27 +0100

    test: Tooltip is not displayed if it is not different from data value

--- packages/visualizations-react/test/Kpi.test.tsx
+++ packages/visualizations-react/test/Kpi.test.tsx
@@ -52,7 +52,6 @@ test('KPI accepts custom link label', () => {
     expect(sourceLink).toBeInTheDocument();
 });
 
-
 test('Tooltip is displayed with 3 digits', async () => {
     render(<KpiCard data={{ value: 42.897654 }} options={options} />);
     const kpiValue = screen.getByText(/43/i);
@@ -63,5 +62,17 @@ test('Tooltip is displayed with 3 digits', async () => {
     fireEvent(kpiValue, mouseenter);
     const tooltip = screen.getByRole('tooltip');
     expect(tooltip).toHaveTextContent('42,898'); 
+});
+
+test('Tooltip is not displayed if it is not different from data value', async () => {
+    render(<KpiCard data={{ value: 43 }} options={options} />);
+    const kpiValue = screen.getByText(/43/i);
+    const mouseenter = new MouseEvent("mouseenter", {
+        bubbles: false,
+        cancelable: false
+      });
+    fireEvent(kpiValue, mouseenter);
+    const tooltip = screen.queryByRole('tooltip');
+    expect(tooltip).toBe(null); 
 });
 
