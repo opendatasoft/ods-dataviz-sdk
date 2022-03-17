3db90fd8f08a290973fcff952759c12c6af83179
Benoit Richter <benoit.richter@opendatasoft.com>
2022-03-17 17:04:37 +0100

    Fix the type of data parameter in Choropleth

--- packages/visualizations/src/components/Map/index.ts
+++ packages/visualizations/src/components/Map/index.ts
@@ -1,10 +1,10 @@
-import type { ChoroplethOptions } from '../types';
+import type { ChoroplethOptions, DataFrame } from '../types';
 import ChoroplethImpl from './Choropleth.svelte';
 import SvelteImpl from '../SvelteImpl';
 
 import 'maplibre-gl/dist/maplibre-gl.css';
 
-export default class Choropleth extends SvelteImpl<string, ChoroplethOptions> {
+export default class Choropleth extends SvelteImpl<DataFrame, ChoroplethOptions> {
     protected getSvelteComponentClass(): typeof ChoroplethImpl {
         return ChoroplethImpl;
     }
