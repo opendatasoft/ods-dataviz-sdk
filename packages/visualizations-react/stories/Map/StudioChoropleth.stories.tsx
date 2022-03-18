be8f3f44f1d2276cba0d952f81e2e111c4231afa
RafaelSzmarowski <rafael.szmarowski@opendatasoft.com>
2022-03-18 16:45:26 +0100

    Add choropleth storybook story

---
+++ packages/visualizations-react/stories/Map/StudioChoropleth.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { Choropleth, Props } from '../../src';
+import { ChoroplethOptions, DataFrame } from '@opendatasoft/visualizations';
+import { shapes } from './shapes';
+
+const meta: Meta = {
+    title: 'Map/Choropleth',
+    component: Choropleth,
+};
+
+export default meta;
+const Template = (args: Props<string, ChoroplethOptions>) => (
+    <div
+        style={{
+            width: '60%',
+            minHeight: '100px',
+            minWidth: '100px',
+            margin: 'auto',
+            border: '1px solid black',
+
+        }}
+    >
+        <Choropleth {...args} />
+    </div>
+);
+export const StudioChoropleth = Template.bind({});
+const StudioChoroplethArgs: Props<DataFrame, ChoroplethOptions> = {
+    data: {
+        error: undefined,
+        loading: false,
+        value: [{x: 'France', y: 35}, {x: 'Corsica', y: 95}],
+    },
+    options: {
+        style: {
+            something: true,
+            color: "#222222",
+        },
+        parameters: {},
+        shapes,
+        colorScale: '#222222',
+        aspectRatio: 1,
+        mapType: 'choropleth',
+    },
+};
+StudioChoropleth.args = StudioChoroplethArgs;
