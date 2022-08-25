4745808128261075527d84cd01a8fb9907d46d77
Etienne Burdet <etienne.burdet@gmail.com>
2022-08-25 15:48:51 +0200

    add d3 canvas exemple

--- packages/visualizations/src/components/Map/SVGChoropleth.svelte
+++ packages/visualizations/src/components/Map/SVGChoropleth.svelte
@@ -1,22 +1,14 @@
 <script>
-    import geojsonToSvg from 'geojson-to-svg';
     import gj2Svg from 'geojson2svg';
     import bbox from '@turf/bbox';
+    import rewind from '@mapbox/geojson-rewind';
+
+    import { geoEqualEarth, geoPath } from 'd3-geo';
+
+    const DPR = window.devicePixelRatio || 1;
 
     export let data;
     export let options;
-
-    let shapes = {};
-
-    const findBbox = (bbox, coords) => {
-        const box = [
-            Math.min(bbox[0], coords[0]),
-            Math.min(bbox[1], coords[1]),
-            Math.max(bbox[2], coords[0]),
-            Math.max(bbox[3], coords[1]),
-        ];
-        return box;
-    };
 
     let extent = [180, 90, -180, -90];
 
@@ -37,14 +29,52 @@
     };
 
     const getShapes = async (reg) => {
-        const pop = await getData(reg);
-        console.time('download');
         const res = await fetch(
             `https://data.opendatasoft.com/api/v2/catalog/datasets/georef-france-commune-arrondissement-municipal@public/exports/geojson?where=reg_name=%22${reg}%22`
         );
         const gj = await res.json();
-        console.timeEnd('download');
-        console.time('render');
+        return gj;
+    };
+
+    const projection = geoEqualEarth();
+    let canvasElement;
+
+    const d3canvas = async (reg) => {
+        const gj = rewind(await getShapes(reg), true);
+        const pop = await getData(reg);
+
+        const ctx = canvasElement.getContext('2d');
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 0.1;
+        ctx.fillStyle = 'blue';
+        const makePath = geoPath(projection.fitSize([DPR * 100, DPR * 100], gj), ctx);
+
+        gj.features.forEach((f) => {
+            ctx.globalAlpha = pop[f.properties.com_code[0]];
+            ctx.beginPath();
+            makePath(f);
+            ctx.fill();
+            ctx.stroke();
+        });
+    };
+
+    const d3path = async (reg) => {
+        // const gj = await getShapes(reg);
+        const gj = rewind(await getShapes(reg), true);
+        const pop = await getData(reg);
+
+        const makePath = geoPath(projection.fitSize([100, 100], gj));
+        const paths = gj.features.map((f) => ({
+            path: makePath(f),
+            opacity: pop[f.properties.com_code[0]],
+        }));
+        return paths;
+    };
+
+    const g2svg = async (reg) => {
+        const gj = await getShapes(reg);
+        const pop = await getData(reg);
+
         extent = bbox(gj);
         const converter = gj2Svg({
             mapExtent: {
@@ -61,23 +91,45 @@
                 svg: converter.convert(feature),
             };
         });
-        console.timeEnd('render');
+        console.timeEnd('convert render');
         return shapes;
     };
 
-    let martinique = getShapes('Martinique');
-    let guadeloupe = getShapes('Guadeloupe');
-    let reunion = getShapes('La Réunion');
-    let guyanne = getShapes('Guyane');
-    const droms = [martinique, guadeloupe, guyanne, reunion];
+    $: d3canvas('Martinique');
+    // let d3s = Promise.all([d3path('Martinique'), d3path('Guadeloupe'), d3path('Guyane')]);
+    // let g2s = Promise.all([g2svg('Martinique'), g2svg('Guadeloupe'), g2svg('Guyane')]);
 </script>
+
+<h1>d3-geo canvas</h1>
+<canvas
+    height={DPR * 100}
+    width={DPR * 100}
+    style="width: 100px; height: 100px"
+    bind:this={canvasElement}
+/>
+
+<!-- <h1>d3-geo svg</h1>
+{#await d3s then droms}
+    {#each droms as shapes}
+        <svg
+            class="d3"
+            xmlns="http://www.w3.org/2000/svg"
+            height="100"
+            width="100"
+            viewBox={`0 0 100 100`}
+            style="stroke: black; stroke-width: 0.1px; fill: none;"
+        >
+            {#each shapes as shape}
+                <path d={shape.path} opacity={shape.opacity} fill="blue" />
+            {/each}
+        </svg>
+    {/each}
+{/await}
 
 <h1>Pop DROM</h1>
 <div>
-    {#each droms as drom}
-        {#await drom}
-            <p>downloading</p>
-        {:then shapes}
+    {#await g2s then droms}
+        {#each droms as shapes}
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 style="stroke: black; stroke-width: 0.1px; fill: none;"
@@ -92,10 +144,9 @@
                     </g>
                 {/each}
             </svg>
-        {/await}
-    {/each}
-</div>
-
+        {/each}
+    {/await}
+</div> -->
 <style>
     div {
         display: flex;
