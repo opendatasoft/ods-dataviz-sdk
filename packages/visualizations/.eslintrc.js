ff7c9444597515a9296fe9f6f25022c81ed80033
William Mai <contact@williammai.fr>
2022-08-26 18:21:30 +0200

    build: Stronger ESLint (no longer rely on TSDX for linting)

--- packages/visualizations/.eslintrc.js
+++ packages/visualizations/.eslintrc.js
@@ -3,13 +3,20 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ['airbnb-base', 'airbnb-typescript/base', 'prettier'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb-base',
+        'airbnb-typescript/base',
+        'prettier',
+    ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         project: './tsconfig.json',
         extraFileExtensions: ['.svelte'],
+        tsconfigRootDir: __dirname,
     },
-    plugins: ['svelte3', '@typescript-eslint'],
+    plugins: ['svelte3', '@typescript-eslint', 'prettier'],
     overrides: [
         {
             files: ['*.svelte'],
@@ -29,6 +36,46 @@ module.exports = {
         'svelte3/typescript': true,
     },
     rules: {
+        'max-len': [
+            'error',
+            {
+                code: 120,
+                tabWidth: 4,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true,
+                ignoreComments: true,
+            },
+        ],
+        camelcase: 0,
+        'no-plusplus': 0,
+        // disallow reassignment of function parameters
+        // disallow parameter object manipulation except for specific exclusions
+        'no-param-reassign': [
+            'error',
+            {
+                props: true,
+                ignorePropertyModificationsFor: [
+                    'acc', // for reduce accumulators
+                    'e', // for e.returnvalue
+                ],
+            },
+        ],
+        'object-curly-newline': [
+            'error',
+            {
+                ObjectExpression: {
+                    minProperties: 6,
+                    multiline: true,
+                    consistent: true,
+                },
+                ObjectPattern: {
+                    minProperties: 6,
+                    multiline: true,
+                    consistent: true,
+                },
+            },
+        ],
+        'import/no-named-as-default': 'off',
         'class-methods-use-this': 'off', // False positives with abstract methods
     },
 };
