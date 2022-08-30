7ccdacdf17b5968dfec6dd09fe3fd68a43a413e7
Etienne Burdet <etienne.burdet@gmail.com>
2022-08-30 17:34:37 +0200

    feat: add basic colors

--- packages/visualizations/src/components/Map/SVGChoropleth.svelte
+++ packages/visualizations/src/components/Map/SVGChoropleth.svelte
@@ -1,21 +1,38 @@
 <script lang="ts">
     import { geoEqualEarth, geoPath } from 'd3-geo';
-
+    import { colorShapes, LIGHT_GREY, DARK_GREY } from './utils';
     import type { SvgChoroplethOptions } from './types';
 
-    // export let data;
+    export let data;
     export let options: SvgChoroplethOptions;
-    $: ({ geoJson, emptyValueColor } = options);
+    $: ({ geoJson, colorScale, emptyValueColor = LIGHT_GREY } = options);
 
     let width: number = 0;
     let height: number = 0;
     let paths: string[];
 
+    const defaultColorsScale: ColorsScale = {
+        type: 'gradient',
+        colors: {
+            start: LIGHT_GREY,
+            end: DARK_GREY,
+        },
+    };
+
     $: projection = geoEqualEarth().fitSize([height, width], geoJson.geoJson);
     $: makePath = geoPath(projection);
-    $: paths =
-        geoJson?.geoJson?.features.map((f) => {
-            return makePath(f);
+    $: coloredShapes = colorShapes(
+        geoJson?.geoJson,
+        data.value,
+        defaultColorsScale,
+        LIGHT_GREY
+    );
+    $: paths = coloredShapes?.geoJson.features.map((f) => {
+        console.log(f)
+            return { 
+                path: makePath(f),
+                color: f.properties.color,
+            }
         }) || [];
 </script>
 
@@ -28,7 +45,7 @@
         style="stroke: black; stroke-width: 0.1px; fill: none;"
     >
         {#each paths as path}
-            <path d={path} />
+            <path d={path.path} fill={path.color}/>
         {/each}
     </svg>
 </div>
