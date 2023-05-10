cf125e890a388f287f6e97ea7696d578552fdde2
RafaelSzmarowski <rafael.szmarowski@opendatasoft.com>
2023-05-10 18:10:31 +0200

    Add style to tooltips and aspect ratio for map figure

--- packages/visualizations/src/components/MapPOI/MapRender.svelte
+++ packages/visualizations/src/components/MapPOI/MapRender.svelte
@@ -29,6 +29,9 @@
     $: currentBbox = bbox;
     // Used to render tooltips on hover
     export let renderTooltip: MapRenderTooltipFunctionPOI;
+    // aspect ratio based on width, by default equal to 1
+    export let aspectRatio = 1;
+    $: cssVarStyles = `--aspect-ratio:${aspectRatio};`;
 
     const hoverPopup = new maplibregl.Popup({
         closeOnClick: false,
@@ -159,15 +162,21 @@
     }
 </script>
 
-<figure class="map-card maps-container">
+<figure class="map-card maps-container" style={cssVarStyles}>
     <div class="main">
         <div id="map" bind:this={container} />
     </div>
 </figure>
 
 <style>
-    #map {
+     #map {
         height: 400px;
+    }
+    @supports (aspect-ratio: auto) {
+        #map {
+            height: auto;
+            aspect-ratio: var(--aspect-ratio);
+        }
     }
     .map-card {
         display: flex;
@@ -175,7 +184,21 @@
         margin: 0;
         position: relative;
     }
+    /* To add classes programmatically in svelte we will use a global selector. We place it inside a local selector to obtain some encapsulation and avoid side effects */
+    .map-card :global(.tooltip-on-hover > .maplibregl-popup-content) {
+        border-radius: 6px;
+        box-shadow: 0px 6px 13px rgba(0, 0, 0, 0.26);
+        padding: 13px;
+    }
+    .map-card :global(.tooltip-on-hover .maplibregl-popup-tip) {
+        border-top-color: transparent;
+        border-bottom-color: transparent;
+        border-left-color: transparent;
+        border-right-color: transparent;
+    }
+
     .main {
+        aspect-ratio: var(--aspect-ratio);
         flex-grow: 1;
         position: relative;
         display: block;
