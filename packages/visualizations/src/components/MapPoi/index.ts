61f09ae3015d82a6d6eba8324214dc89bf9e5ff4
Kevin Fabre <kevin.fabre@opendatasoft.com>
2023-07-31 17:21:48 +0200

    refactor: type and lint

--- packages/visualizations/src/components/MapPoi/index.ts
+++ packages/visualizations/src/components/MapPoi/index.ts
@@ -1,11 +1,10 @@
-import type { FeatureCollection } from 'geojson';
-import type { PoiMapOptions } from './types';
+import type { PoiMapData, PoiMapOptions } from './types';
 import PoiGeoJsonImpl from './PoiGeoJson.svelte';
 import SvelteImpl from '../SvelteImpl';
 
 import 'maplibre-gl/dist/maplibre-gl.css';
 
-export default class PoiGeoJson extends SvelteImpl<FeatureCollection, PoiMapOptions> {
+export default class PoiGeoJson extends SvelteImpl<PoiMapData, PoiMapOptions> {
     protected getSvelteComponentClass(): typeof PoiGeoJsonImpl {
         return PoiGeoJsonImpl;
     }
