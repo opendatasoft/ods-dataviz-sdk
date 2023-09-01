c380e9726c0c43bedad321cbd2695f39ce161dc7
Kevin Fabre <kevin.fabre@opendatasoft.com>
2023-09-01 09:38:05 +0200

    feat: add PoiMap component

---
+++ packages/visualizations/src/components/MapPoi/types.ts
@@ -0,0 +1,40 @@
+import type { CircleLayerSpecification, StyleSpecification } from 'maplibre-gl';
+import type { BBox, FeatureCollection } from 'geojson';
+import type { Color } from '../types';
+
+export interface PoiMapOptions {
+    /** Configuration of map style */
+    style?: StyleSpecification | string;
+    /** Configuration for the layers to display Pois */
+    layerParams?: LayerParams;
+    /** Maximum boundaries of the map, outside of which the user cannot zoom/move
+     * Also set the position of the map when rendering.
+     * If undefined, the map will fit the content.
+     */
+    bbox?: BBox | undefined;
+    /** Aspect ratio used to draw the map. The map will take he width available to it, and decide its height based on that ratio. */
+    aspectRatio?: number;
+    /** Is the map interactive for the user (zoom, move, tooltips...)? */
+    interactive?: boolean;
+}
+/** Structure containing the shapes data used by the POI map
+ */
+export type PoiMapDataValue = {
+    value: FeatureCollection;
+};
+
+export type CircleLayer = Omit<CircleLayerSpecification, 'id' | 'source'>;
+
+export type LayerParams = {
+    colors: Color[]; // Array of colors to match the array of values
+    matchValues: string[]; // Array of values to apply colors
+    matchKey: string; // The features key on which apply colors and values mapping
+    noMatchColor?: Color;
+};
+
+export type PoiMapLayer = CircleLayer;
+
+export type GeoPoint = {
+    lat: number;
+    lon: number;
+};
--- packages/visualizations/src/index.ts
+++ packages/visualizations/src/index.ts
@@ -3,6 +3,7 @@ export { default as MarkdownText } from './components/MarkdownText';
 export { default as KpiCard } from './components/KpiCard';
 export { ChoroplethGeoJson, ChoroplethVectorTiles } from './components/Map/WebGl';
 export { default as ChoroplethSvg } from './components/Map/Svg';
+export { default as PoiGeoJson } from './components/MapPoi';
 
 export * from './types';
 export * from './components/types';
@@ -10,4 +11,5 @@ export * from './components/Chart/types';
 export * from './components/KpiCard/types';
 export * from './components/Map/types';
 export * from './components/MarkdownText/types';
+export * from './components/MapPoi/types';
 export * from './components/Legend/types';
