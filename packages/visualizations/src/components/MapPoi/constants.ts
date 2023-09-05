6d63c95c4a004fba580c9d73360441e649d764f0
Kevin Fabre <kevin.fabre@opendatasoft.com>
2023-09-05 14:17:27 +0200

    feat(PoiMap): can display a popup on feature click
    Each layer has its own popup configuration

--- packages/visualizations/src/components/MapPoi/constants.ts
+++ packages/visualizations/src/components/MapPoi/constants.ts
@@ -1,5 +1,5 @@
 import type { BBox } from 'geojson';
-import type { StyleSpecification } from 'maplibre-gl';
+import type { LngLatLike, PopupOptions, StyleSpecification } from 'maplibre-gl';
 
 export const DEFAULT_BASEMAP_STYLE: StyleSpecification = {
     version: 8,
@@ -11,3 +11,11 @@ export const DEFAULT_BASEMAP_STYLE: StyleSpecification = {
 export const DEFAULT_BBOX: BBox = [180, 90, -180, -90];
 
 export const DEFAULT_ASPECT_RATIO = 1;
+
+export const DEFAULT_MAP_CENTER: LngLatLike = [0, 0];
+
+export const POPUP_OPTIONS: PopupOptions = {
+    className: 'poi-map__popup',
+    closeButton: false,
+    maxWidth: '300px',
+};
