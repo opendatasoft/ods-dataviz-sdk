80265d56674fb8221d90f5c3ccde296e66f8aa14
Etienne Burdet <etienne.burdet@hey.com>
2023-09-19 10:11:07 +0200

    lint: run prettier

--- packages/visualizations-react/stories/Poi/PoiMap.stories.tsx
+++ packages/visualizations-react/stories/Poi/PoiMap.stories.tsx
@@ -11,46 +11,50 @@ import { timeout } from '../utils';
 
 const BASE_STYLE = 'https://demotiles.maplibre.org/style.json';
 
-const layer1 : Layer = {
+const layer1: Layer = {
     id: 'layer-001',
-    source: "cities",
-    type: "circle",
+    source: 'cities',
+    type: 'circle',
     color: 'black',
     popup: {
         display: PopupDisplayTypes.Tooltip,
         getContent: async (_, properties) => {
             await timeout(500);
-            const {key} = properties as {key: string};
+            const { key } = properties as { key: string };
             return Promise.resolve(`<h4>${key}</h4>`);
         },
         getLoadingContent: () => 'Loading...',
-    }
+    },
 };
 
-const layer2 : Layer = {
+const layer2: Layer = {
     id: 'layer-002',
-    source: "battles",
-    type: "circle",
+    source: 'battles',
+    type: 'circle',
     color: 'red',
     popup: {
         display: PopupDisplayTypes.Sidebar,
         getContent: async (_, properties) => {
             await timeout(500);
-            const {name, date, description} = properties as {name: string, date: string, description: string};
+            const { name, date, description } = properties as {
+                name: string;
+                date: string;
+                description: string;
+            };
             return Promise.resolve(`<h4>${name}</h4><p>${description}<p/><small>${date}</small>`);
         },
         getLoadingContent: () => 'Loading...',
-    }
+    },
 };
 
 const layers = [layer1, layer2];
 
-const citiesColorMatch =  {
+const citiesColorMatch = {
     key: 'key',
-    colors: {Paris: 'blue', Nantes: 'yellow', Bordeaux: 'purple', Marseille : 'lightblue' }
+    colors: { Paris: 'blue', Nantes: 'yellow', Bordeaux: 'purple', Marseille: 'lightblue' },
 };
 
-const bbox : BBox = [ -6.855469,41.343825,11.645508,51.371780];
+const bbox: BBox = [-6.855469, 41.343825, 11.645508, 51.37178];
 
 const meta: ComponentMeta<typeof PoiMap> = {
     title: 'Poi/PoiMap',
@@ -59,7 +63,7 @@ const meta: ComponentMeta<typeof PoiMap> = {
 
 export default meta;
 
-const Template: ComponentStory<typeof PoiMap> = args => (
+const Template: ComponentStory<typeof PoiMap> = (args) => (
     <div
         style={{
             width: '50%',
@@ -76,19 +80,19 @@ const Template: ComponentStory<typeof PoiMap> = args => (
 /**
  * STORY: No layer params
  */
-export const PoiMapNoLayersParams : ComponentStory<typeof PoiMap> = Template.bind({});
+export const PoiMapNoLayersParams: ComponentStory<typeof PoiMap> = Template.bind({});
 const PoiMapNoLayersParamsArgs = {
     data: {},
-    options: {style: BASE_STYLE, bbox}
+    options: { style: BASE_STYLE, bbox },
 };
 PoiMapNoLayersParams.args = PoiMapNoLayersParamsArgs;
 
 /**
  * STORY: No interactive
  */
-export const PoiMapNonInteractive : ComponentStory<typeof PoiMap> = Template.bind({});
+export const PoiMapNonInteractive: ComponentStory<typeof PoiMap> = Template.bind({});
 const PoiMapNonInteractiveArgs = {
-    data:  {value:{ layers, sources}},
+    data: { value: { layers, sources } },
     options: {
         style: BASE_STYLE,
         bbox,
@@ -100,9 +104,9 @@ PoiMapNonInteractive.args = PoiMapNonInteractiveArgs;
 /**
  * STORY: With match expression
  */
-export const PoiMapMatchExpression : ComponentStory<typeof PoiMap> = Template.bind({});
+export const PoiMapMatchExpression: ComponentStory<typeof PoiMap> = Template.bind({});
 const PoiMapMatchExpressionArgs = {
-    data:  {value:{ layers: [{...layer1, colorMatch: citiesColorMatch}, layer2], sources}},
-    options: {style: BASE_STYLE, bbox },
+    data: { value: { layers: [{ ...layer1, colorMatch: citiesColorMatch }, layer2], sources } },
+    options: { style: BASE_STYLE, bbox },
 };
 PoiMapMatchExpression.args = PoiMapMatchExpressionArgs;
