3d7f1184931927c96eac95030e14ecc83c837294
Etienne Burdet <etienne.burdet@hey.com>
2023-10-12 09:14:36 +0200

    lint, should run chromatic now

--- packages/visualizations/src/index.ts
+++ packages/visualizations/src/index.ts
@@ -5,7 +5,7 @@ export { ChoroplethGeoJson, ChoroplethVectorTiles } from './components/Map/WebGl
 export { default as ChoroplethSvg } from './components/Map/Svg';
 export { default as PoiMap } from './components/MapPoi';
 
-//DONT RUN CI
+// RUN CI
 export * from './types';
 export * from './components/types';
 export * from './components/Chart/types';
