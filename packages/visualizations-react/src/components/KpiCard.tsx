e54cc8f1517e2a51384c3d9567caf2cc896fed7c
Etienne Burdet <etienne.burdet@hey.com>
2023-12-29 15:35:06 +0100

    fix: split into two useEffect to avoid remount

--- packages/visualizations-react/src/components/KpiCard.tsx
+++ packages/visualizations-react/src/components/KpiCard.tsx
@@ -1,20 +1,28 @@
 import { KpiCard as _KpiCard, KpiCardImpl, KpiCardOptions } from '@opendatasoft/visualizations';
-import React, { useLayoutEffect, useRef } from 'react';
+import React, { useLayoutEffect, useEffect, useRef } from 'react';
 
 // Explicit name and type are needed for storybook
 const KpiCard = (props: any) => {
-    const svelteRef = useRef<HTMLDivElement>(null);
-
+    const svelteComponentRef = useRef<any>(null);
+    const mountRef = useRef<HTMLDivElement>(null);
     useLayoutEffect(() => {
-        if (svelteRef?.current) {
-            new KpiCardImpl({
-                target: svelteRef.current,
+        if (mountRef?.current) {
+            const component = new KpiCardImpl({
+                target: mountRef.current,
                 props,
             });
+            svelteComponentRef.current = component;
         }
-    }, [props, svelteRef]);
+    }, [mountRef]);
 
-    return <div ref={svelteRef} />;
+    useEffect(() => {
+        console.log(props);
+        if (svelteComponentRef?.current) {
+            svelteComponentRef.current.$set({ ...props });
+        }
+    }, [props]);
+
+    return <div ref={mountRef} />;
 };
 
 export default KpiCard;
