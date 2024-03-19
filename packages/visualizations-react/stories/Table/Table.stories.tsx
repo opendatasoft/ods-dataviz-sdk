3f482f667c4d22c20e211a9d70424164e9bdca09
Etienne Burdet <etienne.burdet@hey.com>
2024-03-19 16:11:12 +0100

    feat: add custom styling to pagination

--- packages/visualizations-react/stories/Table/Table.stories.tsx
+++ packages/visualizations-react/stories/Table/Table.stories.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { Column, TableOptions, TableData, Async } from '@opendatasoft/visualizations';
+import type { TableData, Async } from '@opendatasoft/visualizations';
 
 import './custom-style.css';
 
 import { Table } from '../../src';
 
 import value from './data';
+import { options } from './config';
 
 const meta: ComponentMeta<typeof Table> = {
     title: 'Table/Table',
@@ -17,65 +18,6 @@ export default meta;
 const data: Async<TableData> = {
     value,
     loading: false,
-};
-
-const columns: Column[] = [
-    {
-        title: 'Title',
-        key: 'title',
-        dataFormat: 'short-text',
-    },
-    {
-        title: 'Price',
-        key: 'price',
-        dataFormat: 'number',
-        options: {
-            style: 'currency',
-            currency: 'EUR',
-        },
-    },
-    {
-        title: 'Content',
-        key: 'content',
-        dataFormat: 'long-text',
-    },
-    {
-        title: 'Published date',
-        key: 'datePublished',
-        dataFormat: 'date',
-        options: { dateStyle: 'full' },
-    },
-    {
-        title: 'Featured',
-        key: 'isFeatured',
-        dataFormat: 'boolean',
-    },
-    {
-        title: 'Word count',
-        key: 'wordCount',
-        dataFormat: 'number',
-    },
-    {
-        title: 'Reading time',
-        key: 'readingTime',
-        dataFormat: 'number',
-    },
-    {
-        title: 'URL',
-        key: 'url',
-        dataFormat: 'url',
-    },
-];
-
-const options: TableOptions = {
-    columns,
-    title: 'My table',
-    subtitle: 'and a subtitle...',
-    description: 'An aria description',
-    source: {
-        href: '',
-    },
-    locale: 'fr',
 };
 
 const Template: ComponentStory<typeof Table> = args => <Table {...args} />;
@@ -113,43 +55,4 @@ export const Unstyled = Template.bind({});
 Unstyled.args = {
     data,
     options: { ...options, unstyled: true },
-};
-
-const sliceData3 = (fullData: Async<TableData>, page: number) => {
-    const startIndex = page * 3;
-    return fullData.value?.slice(startIndex, startIndex + 3);
-};
-
-const PaginatedTemplate: ComponentStory<typeof Table> = args => {
-    const { data: rawData, options: paginatedOptions } = args;
-    const { pages } = paginatedOptions;
-    const { initial = 0 } = pages || {};
-    const [records, setRecords] = useState(sliceData3(rawData, initial || 0));
-
-    if (paginatedOptions.pages && rawData.value) {
-        paginatedOptions.pages.onChangePage = async (page: number) => {
-            await setTimeout(() => {
-                setRecords(sliceData3(rawData, page));
-            }, 1000);
-        };
-    }
-
-    const paginatedData = { value: records, isLoading: false };
-    return <Table data={paginatedData} options={paginatedOptions} />;
-};
-
-const paginatedOptions = {
-    ...options,
-    pages: {
-        initial: 2,
-        recordsPerPage: 3,
-        totalRecords: value.length,
-        onChangePage: () => {}, // set in template
-    },
-};
-
-export const Paginated = PaginatedTemplate.bind({});
-Paginated.args = {
-    data,
-    options: paginatedOptions,
 };
--- packages/visualizations/src/components/Table/Pagination/DoubleRight.svelte
+++ packages/visualizations/src/components/Table/Pagination/DoubleRight.svelte
@@ -1,13 +1,15 @@
 <script>
-    export let color = '#FFFFFF';
 </script>
 
 <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="m10.781 8-3.3-3.3.943-.943L12.667 8l-4.243 4.243-.943-.943 3.3-3.3z" fill={color}/>
-    <path d="m6.633 8.243-3.3-3.3L4.276 4l4.243 4.243-4.243 4.242-.943-.942 3.3-3.3z" fill={color}/>
+    <path d="m10.781 8-3.3-3.3.943-.943L12.667 8l-4.243 4.243-.943-.943 3.3-3.3z" />
+    <path d="m6.633 8.243-3.3-3.3L4.276 4l4.243 4.243-4.243 4.242-.943-.942 3.3-3.3z" />
 </svg>
 
 
 
 <style>
+    path {
+        fill: currentColor;
+    }
 </style>
