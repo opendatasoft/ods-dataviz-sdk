0889a00d1de75fb2d95c416d60a9bf85bd4c2ce2
Etienne Burdet <etienne.burdet@hey.com>
2024-03-20 08:47:18 +0100

    fix: fix typing with new props

--- packages/visualizations/src/components/Table/Pagination/Pagination.svelte
+++ packages/visualizations/src/components/Table/Pagination/Pagination.svelte
@@ -65,4 +65,15 @@
             flex: 0 1 120px;
         }
     }
+
+    :global(.ods-dataviz-sdk-table--default) {
+        .pages {
+            flex-grow: 1;
+        }
+
+        .numbering,
+        .spacer {
+            flex: 0 1 120px;
+        }
+    }
 </style>
