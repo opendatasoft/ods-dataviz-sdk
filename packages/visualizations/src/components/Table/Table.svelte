b103177ac467af3b2c2fdf455c7398f2cbf13d7b
Etienne Burdet <etienne.burdet@hey.com>
2024-03-20 08:20:51 +0100

    feat: make controls stable in height and stylables

--- packages/visualizations/src/components/Table/Table.svelte
+++ packages/visualizations/src/components/Table/Table.svelte
@@ -7,6 +7,7 @@
     import Header from './Header.svelte';
     import Body from './Body.svelte';
     import { updateLocale } from './store';
+    import { MAX_ROW_HEIGHT } from './constants';
 
     // ensure exported type matches declared props
     type $$Props = TableProps;
@@ -17,9 +18,15 @@
     const tableId = `table-${generateId()}`;
 
     $: ({ value: records } = data);
-    $: ({ columns, title, subtitle, description, source, unstyled, locale, pages } = options);
+    $: ({
+     columns, title, subtitle, description, source, unstyled, locale, pages 
+    } = options);
     $: defaultStyle = !unstyled;
     $: updateLocale(locale);
+    /* Preserves paginations controls positioning
+    min heigh of table + controls = max-height of row * (number of rows + 1 for headers)
+    */
+    $: minHeight = pages ? `${MAX_ROW_HEIGHT * (pages.recordsPerPage + 1)}px` : 'none';
 </script>
 
 <div class="ods-dataviz-sdk-table" class:ods-dataviz-sdk-table--default={defaultStyle}>
@@ -33,13 +40,15 @@
             {/if}
         </div>
     {/if}
-    <div class="table-wrapper">
-        <table aria-describedby={description ? tableId : undefined}>
-            <Header {columns} />
-            {#if records}
-                <Body {records} {columns} />
-            {/if}
-        </table>
+    <div class="table-controls" style="--min-height: {minHeight}">
+        <div class="table-wrapper">
+            <table aria-describedby={description ? tableId : undefined}>
+                <Header {columns} />
+                {#if records}
+                    <Body {records} {columns} />
+                {/if}
+            </table>
+        </div>
     </div>
     {#if pages}
         <Pagination {...pages} />
