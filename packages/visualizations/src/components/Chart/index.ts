f07f5cd66395756047241213526a0c65385fec38
Etienne Burdet <etienne.burdet@hey.com>
2024-05-31 15:19:02 +0200

    chore: add svelte route and explicit chartJs imports

--- packages/visualizations/src/components/Chart/index.ts
+++ packages/visualizations/src/components/Chart/index.ts
@@ -1,15 +1,15 @@
-import * as ChartJs from 'chart.js';
+import ChartJs, { Chart as ChartModule, registerables } from 'chart.js';
 import { TreemapController, TreemapElement } from 'chartjs-chart-treemap';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
+import DataLabels from 'chartjs-plugin-datalabels';
 import Stacked100Plugin from 'chartjs-plugin-stacked100';
 import Chart from './Chart.svelte';
 import PieDataLabelsPlugin from './pieDataLabelsPlugin';
 
-ChartJs.Chart.register(...ChartJs.registerables);
-ChartJs.Chart.register(ChartDataLabels);
-ChartJs.Chart.register(PieDataLabelsPlugin);
-ChartJs.Chart.register(Stacked100Plugin);
-ChartJs.Chart.register(TreemapController, TreemapElement);
+ChartModule.register(...registerables);
+ChartModule.register(DataLabels);
+ChartModule.register(PieDataLabelsPlugin);
+ChartModule.register(Stacked100Plugin);
+ChartModule.register(TreemapController, TreemapElement);
 
 ChartJs.defaults.animation = false;
 
@@ -17,5 +17,5 @@ ChartJs.defaults.animation = false;
 // eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
 export const _ChartJs = ChartJs;
 // eslint-disable-next-line @typescript-eslint/naming-convention, no-underscore-dangle
-export const _ChartDataLabels = ChartDataLabels;
+export const _ChartDataLabels = DataLabels;
 export default Chart;
