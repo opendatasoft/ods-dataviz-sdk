010c1a303cb01b918e0bdf3d1496653cd3abb8b3
Kevin Fabre <kevin.fabre@opendatasoft.com>
2024-06-28 10:18:28 +0200

    feat(Table): minimap preview for geo fields

--- packages/visualizations/src/components/Table/Cell/Format/GeoFormat.svelte
+++ packages/visualizations/src/components/Table/Cell/Format/GeoFormat.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+    import { tick } from 'svelte';
     import { WebGlMap } from 'components/Map';
+    import tippy from 'components/utils/tippy';
+
     import type { WebGlMapOptions, WebGlMapData } from 'components/Map';
 
     export let rawValue: unknown;
@@ -8,35 +11,52 @@
     export let sources: (v: unknown) => WebGlMapData['sources'] = () => ({});
     export let layers: (v: unknown) => WebGlMapData['layers'] = () => [];
 
-    let showMap = false;
+    let tooltipContent;
+    let showMap;
 
     $: data = {
         sources: sources(rawValue),
         layers: layers(rawValue),
     };
-
-    function onLabelClick() {
-        showMap = !showMap;
-    }
 </script>
 
-<div>
+<div
+    role="button"
+    tabindex="0"
+    use:tippy={{
+        content: tooltipContent,
+        theme: 'ods-visualization-table',
+        delay: [500, 0],
+        duration: [275, 0],
+        maxWidth: 'none',
+        onTrigger: (instance) => {
+            showMap = true;
+        },
+        onUntrigger: (instance) => {
+            showMap = false;
+        },
+    }}
+>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div class="label" on:click={onLabelClick}>{display(rawValue)}</div>
+    <div class="label">{display(rawValue)}</div>
+    <!-- To run a WebGl instance only when tooltip is visible -->
     {#if showMap}
-        <div class="table-cell-map-container">
+        <div bind:this={tooltipContent} class="table-cell-map-container">
             <WebGlMap options={mapOptions} {data} />
         </div>
     {/if}
 </div>
 
-<style>
+<style lang="scss">
     .label {
         cursor: pointer;
         text-decoration: underline;
     }
-    .table-cell-map-container {
-        width: 100%;
-        height: 250px;
+    :global(.table-cell-map-container) {
+        width: 360px;
+        height: 240px;
+        :global(.ods-visualization__map-container) {
+            border-radius: 3px;
+        }
     }
 </style>
