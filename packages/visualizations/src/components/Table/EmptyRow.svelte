2e6dcc8e98506ec57a692ee2beef29d515732217
Kevin Fabre <kevin.fabre@opendatasoft.com>
2024-07-18 17:46:42 +0200

    style(Table): no records label is always visible

---
+++ packages/visualizations/src/components/Table/EmptyRow.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    export let label = 'No records found...';
+</script>
+
+<tr>
+    <div>
+        <em>{label}</em>
+    </div>
+</tr>
+
+<style lang="scss">
+    $row-height: 48px;
+    tr {
+        height: $row-height;
+    }
+    div {
+        position: absolute;
+        left: 0;
+        right: 0;
+        height: $row-height;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
