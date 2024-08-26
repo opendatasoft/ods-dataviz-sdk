b9987af2d04e457cb079f5a83bcb0bbc0c24a4e1
Kevin Fabre <117300300+KevinFabre-ods@users.noreply.github.com>
2024-08-26 09:28:24 +0200

    fix(Table): "no records" label is always visible (#258)

--- packages/visualizations/src/components/Table/Body.svelte
+++ packages/visualizations/src/components/Table/Body.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
     import type { Column, TableData } from './types';
     import Cell, { LoadingCell } from './Cell';
+    import EmptyRow from './EmptyRow.svelte';
 
     export let loadingRowsNumber: number | null;
     export let columns: Column[];
     export let records: TableData | undefined;
-    export let emptyStateLabel = 'No records found...';
+    export let emptyStateLabel: string | undefined;
 </script>
 
 <tbody>
     {#if records?.length === 0 && !loadingRowsNumber}
-        <tr>
-            <td colspan={columns.length}>
-                <em>{emptyStateLabel}</em>
-            </td>
-        </tr>
+        <EmptyRow label={emptyStateLabel} />
     {/if}
     {#if loadingRowsNumber}
         {#each Array(loadingRowsNumber) as _}
@@ -42,11 +39,5 @@
 
     :global(.ods-dataviz--default) tr:last-child {
         border-bottom: none;
-    }
-
-    :global(.ods-dataviz--default) em {
-        text-align: center;
-        width: 100%;
-        display: block;
     }
 </style>
---
+++ packages/visualizations/src/components/Table/EmptyRow.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let label = 'No records found...';
+</script>
+
+<tr>
+    <td>
+        <em>{label}</em>
+    </td>
+</tr>
+
+<style lang="scss">
+    $row-height: 48px;
+    tr {
+        height: $row-height;
+    }
+    td {
+        box-sizing: border-box;
+        position: absolute;
+        left: 0;
+        right: 0;
+        height: $row-height;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
