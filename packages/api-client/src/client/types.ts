0ce87e04b8600295b156d9e91f87fc2232b3fb88
Kevin Fabre <117300300+KevinFabre-ods@users.noreply.github.com>
2024-09-05 11:20:58 +0200

    * feat(types): update and new types (#259)
    - Dataset has no longer optional keys and new keys has been added such as `alternative_exports`
    - Dataset field types have been defined
    - Enrich request data types with the _links key (If request contains the include_links params)
    - Expose proper constants for export types

--- packages/api-client/src/client/types.ts
+++ packages/api-client/src/client/types.ts
@@ -1,5 +1,8 @@
 // Type hints for Api response
 
+import { ValueOf } from '../utils';
+import { EXPORT_CATALOG_FORMAT, EXPORT_DATASET_FORMAT, ODS_DATASET_FIELD_TYPE } from './constants';
+
 export interface Facet {
     name: string;
     count: number;
@@ -18,91 +21,65 @@ export interface Link {
     rel: string;
 }
 
-export interface OdsDataset {
-    dataset_id?: string;
-    dataset_uid?: string;
-    has_records?: boolean;
-    data_visible?: boolean;
-    features?: string[];
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    metas?: Record<string, any>;
-    fields?: {
-        name?: string;
-        label?: string;
-        type?: string;
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        annotations?: any;
-        description?: string;
-    }[];
+interface DataWithLinks {
+    _links?: Link[];
 }
 
-export interface OdsRecord<T> {
-    id?: string;
-    timestamp?: string;
-    size?: number;
-    fields: T;
+export type DatasetFieldType = ValueOf<typeof ODS_DATASET_FIELD_TYPE>;
+
+export interface DatasetAttachement {
+    id: string;
+    mimetype: string;
+    title: string;
+    url: string;
 }
 
-export interface ApiRecords<T> {
-    total_count?: number;
-    links: Link[];
-    results: OdsRecord<T>[];
+export interface DatasetAlternativeExport extends DatasetAttachement {
+    description: string;
 }
 
-export interface ApiDatasets {
-    total_count: number;
-    links: Link[];
-    results: OdsDataset[];
+export interface DatasetField {
+    description: string | null;
+    name: string;
+    label: string;
+    type: DatasetFieldType;
+    annotations: unknown;
 }
+
+export type Dataset = DataWithLinks & {
+    dataset_id: string;
+    dataset_uid: string;
+    has_records: boolean;
+    data_visible: boolean;
+    features: string[];
+    metas: object;
+    fields: DatasetField[];
+    visibility: 'restricted' | 'domain';
+    attachements: DatasetAttachement[];
+    alternative_exports: DatasetAlternativeExport[];
+};
+
+export type DatasetRecord<T extends object> = T & DataWithLinks;
 
 export interface ApiFacets {
     links: Link[];
     facets: FacetRoot[];
 }
 
-export interface ApiQuery<T> {
+export type ApiQuery<T extends object> = DataWithLinks & {
     total_count?: number;
     results: T[];
-}
+};
+
+export type ApiCatalog<Shape extends object = Dataset> = ApiQuery<Shape> & { total_count: number };
+export type ApiDatasetRecords<R extends object> = ApiQuery<DatasetRecord<R>> & {
+    total_count: number;
+};
 
 export interface ApiExport<T> {
     [key: string]: T;
 }
-export const EnumExportCatalogFormat = {
-    CSV: 'csv',
-    JSON: 'json',
-    XLSX: 'xlsx',
-    RDF: 'rdf',
-    TTL: 'ttl',
-    DATA_JSON: 'data.json',
-    RSS: 'rss',
-    DCAT: 'dcat',
-    DCAT_AP_CH: 'dcat-ap-ch',
-    DCAT_AP_IT: 'dcat-ap-it',
-    DCAT_AP_DE: 'dcat-ap-de',
-    DCAT_AP_SE: 'dcat-ap-se',
-    DCAT_AP_SP: 'dcat-ap-sp',
-    DCAT_AP_V1: 'dcat-ap-v1',
-    DCAT_AP_BENAP: 'dcat_ap_benap',
-} as const;
 
-export type ExportCatalogFormat =
-    typeof EnumExportCatalogFormat[keyof typeof EnumExportCatalogFormat];
+export type ExportCatalogFormat = ValueOf<typeof EXPORT_CATALOG_FORMAT>;
 
-export const EnumExportDatasetFormat = {
-    JSON: 'json',
-    GEOJSON: 'geojson',
-    SHP: 'shp',
-    CSV: 'csv',
-    XLSX: 'xlsx',
-    KML: 'kml',
-    JSONLD: 'jsonld',
-    JSONL: 'jsonl',
-    RDFXML: 'rdfxml',
-    TURTLE: 'turtle',
-    N3: 'n3',
-    MVT: 'mvt',
-} as const;
-
-export type ExportDatasetFormat =
-    typeof EnumExportDatasetFormat[keyof typeof EnumExportDatasetFormat];
+export type ExportDatasetFormat = ValueOf<typeof EXPORT_DATASET_FORMAT>;
--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -6,5 +6,6 @@
 export * from './client';
 export * from './client/error';
 export * from './client/types';
+export * from './client/constants';
 export * from './odsql';
 export * from './odsql/clauses';
