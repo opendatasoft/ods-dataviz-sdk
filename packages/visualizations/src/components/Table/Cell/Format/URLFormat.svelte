a1e901c3a956618b8643a0ed705740a1249454a1
Etienne Burdet <etienne.burdet@hey.com>
2024-10-30 18:43:18 +0100

    chore: add stricter url check and extend fallback

--- packages/visualizations/src/components/Table/Cell/Format/URLFormat.svelte
+++ packages/visualizations/src/components/Table/Cell/Format/URLFormat.svelte
@@ -1,28 +1,29 @@
 <script lang="ts">
     import ShortTextFormat from './ShortTextFormat.svelte';
+    import { isValidUrl } from './utils';
 
     export let rawValue: unknown;
     export let display = (v: string) => v;
     export let target: HTMLAnchorElement['target'] = '_blank';
     export let rel = 'nofollow noreferrer noopener';
 
-    function format(v: unknown) {
-        try {
-            // eslint-disable-next-line no-new
-            new URL(v as string);
-            return display(v as string);
-        } catch (_) {
-            // eslint-disable-next-line no-console
-            console.warn(`ODS Dataviz SDK - Table: no url detected, formatting as string`);
-            return null;
+    $: format = (v: unknown) => {
+        if (isValidUrl(v)) {
+            return {
+                text: display(v),
+                href: v as string,
+            };
         }
-    }
+        // eslint-disable-next-line no-console
+        console.warn(`ODS Dataviz SDK - Table: no url detected, formatting as string`);
+        return { text: null, href: null };
+    };
 
-    $: value = format(rawValue);
+    $: ({ text, href } = format(rawValue));
 </script>
 
-{#if value}
-    <a href={value} {rel} {target}>{display(value)}</a>
+{#if text}
+    <a {href} {rel} {target}>{text}</a>
 {:else}
-    <ShortTextFormat {rawValue} />
+    <ShortTextFormat {rawValue} {display} />
 {/if}
