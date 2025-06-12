49c8f58a8ff4cfd0fde48684223a69aa93208209
Kevin Fabre <kevin.fabre@opendatasoft.com>
2025-06-12 09:56:27 +0200

    fix(table): update page size select spacing on RTL

--- packages/visualizations/src/components/Pagination/PageSize.svelte
+++ packages/visualizations/src/components/Pagination/PageSize.svelte
@@ -34,14 +34,14 @@
         width: fit-content;
         /* remove all styles and re-add arrow to fix Safari appearance */
         background: url('data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA0Ljk1IDEwIj48ZGVmcz48c3R5bGU+LmNscy0xe2ZpbGw6I2ZmZjt9LmNscy0ye2ZpbGw6IzQ0NDt9PC9zdHlsZT48L2RlZnM+PHRpdGxlPmFycm93czwvdGl0bGU+PHJlY3QgY2xhc3M9ImNscy0xIiB3aWR0aD0iNC45NSIgaGVpZ2h0PSIxMCIgb3BhY2l0eT0iMCIvPjxwb2x5Z29uIGNsYXNzPSJjbHMtMiIgcG9pbnRzPSIxLjQxIDQuNjcgMi40OCAzLjE4IDMuNTQgNC42NyAxLjQxIDQuNjciLz48cG9seWdvbiBjbGFzcz0iY2xzLTIiIHBvaW50cz0iMy41NCA1LjMzIDIuNDggNi44MiAxLjQxIDUuMzMgMy41NCA1LjMzIi8+PC9zdmc+')
-            no-repeat 95% 50%;
+            no-repeat calc(100% - var(--spacing-50)) 50%;
         -moz-appearance: none;
         -webkit-appearance: none;
         appearance: none;
-        padding-inline-end: var(--spacing-100);
+        padding-inline-end: var(--spacing-200);
     }
 
     :global(html[dir='rtl'] .ods-dataviz--default) select {
-        background-position: 5%;
+        background-position: var(--spacing-50) 50%;
     }
 </style>
