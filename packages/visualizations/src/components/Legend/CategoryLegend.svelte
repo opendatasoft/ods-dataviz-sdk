f1ed0c06613c4ab7b18433fd5a6450203830470b
RafaelSzmarowski <rafael.szmarowski@opendatasoft.com>
2025-09-18 11:22:31 +0200

    feat: category legend groups item per variant

--- packages/visualizations/src/components/Legend/CategoryLegend.svelte
+++ packages/visualizations/src/components/Legend/CategoryLegend.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import CategoryLegendItem from './CategoryLegend/Item/CategoryLegendItem.svelte';
-    import type { CategoryLegend, CategoryItem } from './types';
+    import { type CategoryLegend, type CategoryItem, CATEGORY_ITEM_VARIANT } from './types';
 
     export let legendOptions: CategoryLegend;
 
@@ -17,6 +17,22 @@
     };
 
     $: ({ items, title, align = 'center' } = legendOptions);
+
+    // group items by available "variant"
+    $: grouped = items.reduce((acc, item) => {
+        const v = item.variant;
+        if (!acc[v]) acc[v] = [];
+        acc[v].push(item);
+        return acc;
+    }, {} as Record<string, CategoryItem[]>);
+
+    const variantOrder = [
+        CATEGORY_ITEM_VARIANT.Circle,
+        CATEGORY_ITEM_VARIANT.Line,
+        CATEGORY_ITEM_VARIANT.Box,
+        CATEGORY_ITEM_VARIANT.Image,
+    ];
+    $: groupKeys = variantOrder.filter(v => grouped[v]);
 </script>
 
 <div class="legend-container" style="--align: {align}">
@@ -24,13 +40,17 @@
         <div class="legend-title">{title}</div>
     {/if}
     <div class="legend-items-container">
-        {#each items as item, i}
-            <CategoryLegendItem
-                {item}
-                itemIndex={i}
-                {toggleSerie}
-                refined={isRefined(i, refinedSeries)}
-            />
+        {#each groupKeys as group}
+            <div class="legend-items-group">
+                {#each grouped[group] as item, i}
+                    <CategoryLegendItem
+                        {item}
+                        itemIndex={i}
+                        {toggleSerie}
+                        refined={isRefined(i, refinedSeries)}
+                    />
+                {/each}
+            </div>
         {/each}
     </div>
 </div>
@@ -49,9 +69,17 @@
     }
     .legend-items-container {
         display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(120px, max-content));
         justify-content: var(--align);
-        grid-gap: 3px 13px;
-        grid-template-columns: repeat(auto-fit, minmax(120px, max-content));
-        padding: 13px 0;
+        gap: 6px 13px;
+        padding: 7px 0;
+    }
+    .legend-items-group {
+        display: grid;
+        grid-template-columns: subgrid;
+        grid-column: 1 / -1;
+        gap: 3px 13px;
+        justify-content: start; /* items always align left in each row */
+        padding: 6px 0;
     }
 </style>
